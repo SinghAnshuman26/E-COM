ca139606f030f789c7009d6e314353ee04be0767
Anshuman <singhanshumanxxz@gmail.com>
2023-12-16 22:39:34 +0530

    final commit

---
+++ db/User.js
@@ -0,0 +1,10 @@
+require("dotenv").config()
+const mongoose =require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
+});
+module.exports = mongoose.model("users",userSchema);
